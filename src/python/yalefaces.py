6ff65cfd35705bc52f01d9eb7141c3edfde5f4e0
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-06-28 02:29:36 -0300

    Compress images with PCA

--- src/python/yalefaces.py
+++ src/python/yalefaces.py
@@ -7,9 +7,13 @@ SHAPE=(243, 320)
 def _get_filepaths(folder_path: str):
     return [os.path.join(folder_path, f) for f in os.listdir(folder_path)]
 
-def _to_image(filepath: str):
+def _to_image(filepath: str, flatten: bool):
     image_pil = Image.open(filepath)
-    return np.array(image_pil, 'uint8').flatten()
+    image = np.array(image_pil, 'uint8')
+
+    if flatten:
+        return image.flatten()
+    return image
 
 def _to_label(filepath: str):
     # "/home/yalefaces/subject03.sad.gif" -> "subject03.sad"
@@ -24,10 +28,14 @@ def _to_label(filepath: str):
     # "03" -> 2
     return int(subject_id) - 1
 
-def load(folder_path: str):
+def load(folder_path: str, flatten=True):
     files = _get_filepaths(folder_path)
 
-    X = np.array(list(map(_to_image, files)))
+    # for CNNs we might not want to flatten images,
+    # images, so we added this optional parameter.
+    flatten_array = [flatten] * len(files)
+
+    X = np.array(list(map(_to_image, files, flatten_array)))
     y = np.array(list(map(_to_label, files)))
 
     return X, y
