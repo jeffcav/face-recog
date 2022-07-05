35772eae565d8926205ddd665792664a7725a348
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-07-05 14:08:38 -0300

    CNN

--- src/python/models/lenet.py
+++ src/python/models/lenet.py
@@ -5,12 +5,14 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras import backend as K
+from keras.optimizers import SGD
 
 # reference: https://pyimagesearch.com/2016/08/01/lenet-convolutional-neural-network-in-python/
 class LeNet:
 	@staticmethod
-	def build(numChannels, imgRows, imgCols, numClasses, activation="relu", weightsPath=None):
-		
+	def build(numChannels, imgRows, imgCols, numClasses, activation="relu", weightsPath=None, num_filters=[24, 32], filter_sizes=[5, 3], dense_width=256):
+		# K.clear_session()
+
 		model = Sequential()
 		inputShape = (imgRows, imgCols, numChannels)
 
@@ -18,34 +20,26 @@ class LeNet:
 		if K.image_data_format() == "channels_first":
 			inputShape = (numChannels, imgRows, imgCols)
 
-        # define the first set of CONV => ACTIVATION => POOL layers
-		model.add(Conv2D(24, 3, padding="same", input_shape=inputShape))
-		model.add(Activation(activation))
+		# define the first set of CONV => ACTIVATION => POOL layers
+		model.add(Conv2D(num_filters[0], filter_sizes[0], padding="same", input_shape=inputShape, activation=activation))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        # define the second set of CONV => ACTIVATION => POOL layers
-		model.add(Conv2D(48, 5, padding="same"))
-		model.add(Activation(activation))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
-        # define the second set of CONV => ACTIVATION => POOL layers
-		model.add(Conv2D(64, 7, padding="same"))
-		model.add(Activation(activation))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
+		for i in range(1, len(num_filters)):
+			# define the first set of CONV => ACTIVATION => POOL layers
+			model.add(Conv2D(num_filters[i], filter_sizes[i], padding="same", activation=activation))
+			model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+		
         # define the first FC => ACTIVATION layers
 		model.add(Flatten())
-		model.add(Dense(800))
-		model.add(Activation(activation))
+		model.add(Dense(dense_width, activation=activation))
 		
         # define the second FC layer
-		model.add(Dense(numClasses))
-		
-        # lastly, define the soft-max classifier
-		model.add(Activation("softmax"))
+		model.add(Dense(numClasses, activation='softmax'))
 
         # if a weights path is supplied (inicating that the model was pre-trained), then load the weights
 		if weightsPath is not None:
 			model.load_weights(weightsPath)
+
+		model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
 		
 		return model
